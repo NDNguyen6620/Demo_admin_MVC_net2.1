db0eeae0e2bba69a3cdbe25e28b90411f653af48
NDNguyen6620 <nd.nguyen6620@gmail.com>
2023-05-09 16:18:27 +0700

    Add project files.

---
+++ DemoNet2.1/Areas/Admin/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoNet2.Areas.Admin.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+        public double SalePrice { get; set; }
+        public string Content { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ DemoNet2.1/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DemoNet2.Areas.Admin.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoNet2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
